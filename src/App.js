e4f9ba5cb98846baae3ba84dfeb68b152163b9f7
emmanuel <gbengacode>
2022-08-07 10:09:34 +0100

    Clean up add offline persistence

--- src/App.js
+++ src/App.js
@@ -1,41 +1,15 @@
-import { useRef, useState } from 'react';
-import './App.css';
-import useRandomJoke from './useRandomJoke';
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Jokes from "./pages/Jokes";
 
 function App() {
-
-  const firstNameRef = useRef(null);
-  const lastNameRef = useRef(null);
-
-  const [firstName, setFirstName] = useState('');
-  const [lastName, setLastName] = useState('');
-  const joke = useRandomJoke(firstName, lastName);
-
-  const generateJoke = (e) =>{
-    e.preventDefault();
-    setFirstName(firstNameRef.current.value);
-    setLastName(lastNameRef.current.value);
-  }
-
-  function refreshPage() {
-    window.location.reload(false);
-  }
-
   return (
-    <div className='App'>
-      <h1>The joke generators</h1>
-      <h2>{joke}</h2>
-      <form>
-       
-        {/* <input placeholder='first name' value={firstName} onChange={e => setFirstName(e.target.value)}    before useref/>
-        <input placeholder='last name' value={lastName} onChange={e => setLastName(e.target.value)}/> */}
-        <input placeholder='first name' ref={firstNameRef}/>
-        <input placeholder='last name' ref={lastNameRef}/>
-        <button onClick={generateJoke}>Click to generate</button>
-        
-      </form>
-      <button onClick={refreshPage}>Click for new Joke</button>
-    
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Jokes />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
